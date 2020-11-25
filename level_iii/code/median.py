63b3e64845b35f851eb4580d457389e42e2e0dfc
Peter Drake <drake@lclark.edu>
2020-11-24 23:24:01 -0800

    Selection sort, which is easier to implement with the tools at hand.

--- level_iii/code/median.py
+++ level_iii/code/median.py
@@ -1,21 +1,4 @@
-numbers = [4, 2, 8, 5, 6]
-
-# Sort the list of numbers
-n = len(numbers)
-for i in range(1, n):
-    # Find index of smallest remaining number
-    min_index = 0
-    for j in range(1, len(numbers)):
-        if numbers[j] < numbers[min_index]:
-            min_index = j
-    # Add it to sorted
-    sorted += [numbers[min_index]]
-    # Remove it from numbers
-    remaining = []
-    for j in range(0, len(numbers)):
-        if j != min_index:
-            remaining += [numbers[j]]
-    numbers = remaining
+sorted = [2, 4, 5, 6, 8]
 
 # Find the median
 n = len(sorted)
--- level_iii/code/sort.py
+++ level_iii/code/sort.py
@@ -1,20 +1,13 @@
 numbers = [2, 1, 8, 7]
-numbers = [8,6,7,5,3,0,9]
 n = len(numbers)
-for i in range(1, n):
-    moving = numbers[i]
-    print(moving)
-    for j in range(i, 0, -1):
-        print(moving)
-        print(numbers[j-1])
-        if moving < numbers[j - 1]:
-            print('moving')
-            numbers[j] = numbers[j - 1]
-            print(numbers)
-        else:
-            j += 1
-            break
-    numbers[j - 1] = moving
-    print(numbers)
+for i in range(0, n):
+    # Find the smallest number at index i or later
+    min = i
+    for j in range(i + 1, n):
+        if numbers[j] < numbers[min]:
+            min = j
+    # Swap that number with the one at index i
+    temp = numbers[min]
+    numbers[min] = numbers[i]
+    numbers[i] = temp
 print(numbers)
-
